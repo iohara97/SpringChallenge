57be938c4c5c62472b85485a15f188bc65691966
nkutomimeli <nathan.kutomi@mercadolivre.com>
2021-12-29 21:43:58 -0300

    InsertProduto()

--- src/main/java/com/br/meli/springchallenge/Database/Database.java
+++ src/main/java/com/br/meli/springchallenge/Database/Database.java
@@ -170,67 +170,133 @@ public class Database {
 
             String query = "INSERT INTO produto (name, category, brand, price, quantity, free_shipping, prestige) VALUES ";
 //            String subquery = "";
-            for (Produto elem:produtos) {
+            List<Integer> keysList = new ArrayList<>();
+            List<Produto> produtosA = new ArrayList<Produto>();
 
-                String subquery = " ('" + elem.getName() + "', " +
+            for (Produto elem : produtos) {
+
+                String subquery = query +
+
+                        " ('" + elem.getName() + "', " +
                         "'" + elem.getCategory() + "', " +
                         "'" + elem.getBrand() + "', " +
                         elem.getPrice().toString() + ", " +
                         elem.getQuantity() + ", " +
                         elem.getFreeShipping().toString() + ", " +
-                        "'"+ elem.getPrestige() + "'),";
+                        "'" + elem.getPrestige() + "')";
 
-                query += subquery;
+                PreparedStatement stmt = cn.prepareStatement(subquery, Statement.RETURN_GENERATED_KEYS);
+
+                int i = stmt.executeUpdate(); //executeQuery();
+
+                ResultSet keysSet = stmt.getGeneratedKeys();
+
+                while (keysSet.next()) {
+                    int key = keysSet.getInt(1);
+
+                    PreparedStatement getStmt = cn.prepareStatement("Select * from produto where productId = ?");
+                    getStmt.setInt(1, key);
+                    ResultSet resultSet = getStmt.executeQuery();
+
+                    while (resultSet.next()) {
+                        // Tentando fazer o cast da linha para o objeto Produto
+//                Produto produto = (Produto) resultSet.getRow();
+
+                        Produto produto = new Produto(
+                                resultSet.getLong("productId"),
+                                resultSet.getString("name"),
+                                resultSet.getString("category"),
+                                resultSet.getString("brand"),
+                                resultSet.getBigDecimal("price"),
+                                resultSet.getInt("quantity"),
+                                resultSet.getBoolean("free_shipping"),
+                                resultSet.getString("prestige")
+                        );
+                        produtosA.add(produto);
+                    }
+
+
+                }
+
             }
 
+            return produtosA;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
             // Retirando o último caracter
-            StringBuilder sb = new StringBuilder(query);
-            sb.deleteCharAt(query.length() - 1);
+//            StringBuilder sb = new StringBuilder(query);
+//            sb.deleteCharAt(query.length() - 1);
 
 //        System.out.println(sb.toString());
 
-            sb.append(" RETURNING *");
-            String query1 = sb.toString();
+//            sb.append(" RETURNING *");
+//            String query1 = sb.toString();
             // lendo os registros
             //Statement statement = cn.createStatement();
 
-            Statement statement = cn.createStatement();
+//            Statement statement = cn.createStatement();
 
 
             // tentar implementar a parametrização da query
 //            PreparedStatement stmt = cn.prepareStatement("Select * from produtos where produtId = ?");
 //            stmt.setInt(1, 4);
 
-            // ResultSet resultSet = statement.executeQuery(query1);
+//             ResultSet resultSet = statement.executeQuery(query1);
 
-            PreparedStatement stmt = cn.prepareStatement(query1);
+//            PreparedStatement stmt = cn.prepareStatement(query1, Statement.RETURN_GENERATED_KEYS);
+//
+//            ResultSet resultSet = stmt.executeUpdate(); //executeQuery();
+//
+//            ResultSet keysSet = null;
+//            keysSet = statement.getGeneratedKeys();
+//
+//
+//            String getQuery = "Select * from produtos where productId in (";
+//
+//            while (keysSet.next()) {
+//                getQuery += keysSet.getInt(1) + ", ";
+//            }
+//
+//            StringBuilder sb1 = new StringBuilder(getQuery);
+//            sb1.deleteCharAt(sb1.length() - 1);
+//            sb1.deleteCharAt(sb1.length() - 1);
+//
+//            sb1.append(") order by name;");
+//
+//            // lendo os registros
+//            PreparedStatement getStmt = cn.prepareStatement(sb1.toString());
+//            ResultSet resultSet = getStmt.executeQuery();
+//
+//
+//            List<Produto> produtosA = new ArrayList<Produto>();
+//
+//            while (resultSet.next()) {
+//                // Tentando fazer o cast da linha para o objeto Produto
+////                Produto produto = (Produto) resultSet.getRow();
+//
+//                Produto produto = new Produto(
+//                        resultSet.getLong("productId"),
+//                        resultSet.getString("name"),
+//                        resultSet.getString("category"),
+//                        resultSet.getString("brand"),
+//                        resultSet.getBigDecimal("price"),
+//                        resultSet.getInt("quantity"),
+//                        resultSet.getBoolean("free_shipping"),
+//                        resultSet.getString("prestige")
+//                );
+//                produtosA.add(produto);
+//            }
 
-            ResultSet resultSet = stmt.executeQuery(); //executeQuery();
-
-            List<Produto> produtosA = new ArrayList<Produto>();
-
-            while (resultSet.next()) {
-                // Tentando fazer o cast da linha para o objeto Produto
-//                Produto produto = (Produto) resultSet.getRow();
-
-                Produto produto = new Produto(
-                        resultSet.getLong("productId"),
-                        resultSet.getString("name"),
-                        resultSet.getString("category"),
-                        resultSet.getString("brand"),
-                        resultSet.getBigDecimal("price"),
-                        resultSet.getInt("quantity"),
-                        resultSet.getBoolean("free_shipping"),
-                        resultSet.getString("prestige")
-                );
-                produtosA.add(produto);
-            }
-
-            return produtosA;
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
+//            return produtosA;
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
